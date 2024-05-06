49b82510a4b19b04122bda20a2a5cf7910bd689b
Jerry Takou <vianneytakou@gmail.com>
2024-05-06 04:01:52 +0200

    f

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,9 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CoreModule, BootstrapComponent, RouterModule } from '@c8y/ngx-components';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  imports: [BrowserAnimationsModule, RouterModule.forRoot([]), CoreModule.forRoot()],
+  bootstrap: [BootstrapComponent]
 })
-export class AppModule { }
+export class AppModule {}
